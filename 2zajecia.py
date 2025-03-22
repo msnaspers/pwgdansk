2a23dd8bcfb9fb1f34fe9e3d0fa761f028fc715b
msnaspers <msnaspers@gmail.com>
2025-03-22 09:25:56 +0100

    go

--- 2zajecia.py
+++ 2zajecia.py
@@ -1,4 +1,23 @@
 import numpy as np
 import math as m
+import random as r
 
-print(m.sqrt(4))
+a = r.randint(1,6)
+
+b=  r.randint(1,6)
+
+print(a,b,a+b)
+def a(n:int):
+
+    yield n
+    yield n+1
+gen = a(1)   
+next(gen)
+
+def infinite_sequence():
+    num = 0
+    while True:
+        yield num
+        num += 1
+a=infinite_sequence()
+print(next(a))
---
+++ 3zajecia.py
@@ -0,0 +1,7 @@
+def main() -> int:
+    print("fff")
+
+if __name__ == '__main__':
+    pass
+#wyonaj main z tego modulu tylko, bardziej jako entry point np w docker
+
