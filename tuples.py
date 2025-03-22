5a4358e5af08a1d146d18612ba41851126ef94a6
msnaspers <msnaspers@gmail.com>
2025-03-22 11:53:13 +0100

    go

--- tuples.py
+++ tuples.py
@@ -2,3 +2,12 @@ a=['lista', 'list']
 a_tuple=(1,2,3,3,3)
 
 print(a_tuple)
+ ##rozpakowywanie krotki
+
+warzywa = ('pomidor', 'ogor', 'kapucha')
+
+(multicolor,*zolty)=warzywa
+
+print(zolty)
+
+(a,b)=1,4
